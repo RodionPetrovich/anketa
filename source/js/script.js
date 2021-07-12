691bec7d66bda7ec5e2b06acf164e42be417a9a4
Родион <rodegen14@gmail.com>
2021-07-12 21:03:42 +0900

    Прописал скрипт на меню и дату рождения, подправил стили

--- source/js/script.js
+++ source/js/script.js
@@ -6,3 +6,19 @@ if (iconMenu) {
     navMenu.classList.toggle('_active');
   })
 }
+
+let bDate = document.querySelector('.user-form__date');
+let arrow = document.querySelector('.arrow');
+if (bDate) {
+  arrow.addEventListener("click", function (e) {
+    bDate.classList.toggle('visually-hidden');
+  })
+};
+
+let field = document.querySelector('input[id="birthday-date"]');
+if (bDate) {
+  field.addEventListener("click", function (e) {
+    bDate.classList.toggle('visually-hidden');
+  })
+};
+
